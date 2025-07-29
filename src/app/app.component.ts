355b134f86a2afef7e2672059c0bb0839ee0bda8
lanamaiumy <alana.maiumy@gmail.com>
2025-07-29 10:56:18 -0300

    Atualização de Layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,10 +12,8 @@ export class AppComponent {
   technologies = [
     { name: 'Angular', iconClass: 'devicon-angularjs-plain', color: '#e57373' },
     { name: 'TypeScript', iconClass: 'devicon-typescript-plain', color: '#64b5f6' },
+    { name: 'Ruby on Rails', iconClass: 'devicon-rails-plain', color: '#e55454ff' },
     { name: 'PostgreSQL', iconClass: 'devicon-postgresql-plain', color: '#7986cb' },
-    { name: 'Vue.js', iconClass: 'devicon-vuejs-plain', color: '#81c784' },
-    { name: 'Laravel', iconClass: 'devicon-laravel-plain', color: '#f06292' },
-    { name: 'PHP', iconClass: 'devicon-php-plain', color: '#b39ddb' },
     { name: 'JavaScript', iconClass: 'devicon-javascript-plain', color: '#fff176' }
   ];
 
